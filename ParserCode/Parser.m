824dcda7eb700ee9d717995e01127593ffa6d38b
Kelsey Justis <kelseyjustis@aol.com>
2015-02-15 18:07:41 -0500

    rudimentary word occurrence detection

--- ParserCode/Parser.m
+++ ParserCode/Parser.m
@@ -1,7 +1,8 @@
 % Cite her: http://www.mathworks.com/matlabcentral/fileexchange/19505-wordcount/content/wordcount.m
+%Folder of converted syllabi files from pdf to text format.
 FolderOfSyllabiTxtFiles = dir('../PDFTextExtractionCode/TEST/TEST/SyllabiTxtFiles/*.txt');
 
-
+% Loop over each syllabus text file
 for fileNumber = 1:length(FolderOfSyllabiTxtFiles)
    CurrTxtFileName = FolderOfSyllabiTxtFiles(fileNumber).name;
    fprintf(['Processing ' CurrTxtFileName ' . . .' '\n']);
@@ -10,31 +11,52 @@ for fileNumber = 1:length(FolderOfSyllabiTxtFiles)
    CurrSylabusWords = textscan(CurrSyllabusTextFile, '%s');
    StopWordsTextFile = fopen('../OutsideVocabDatabases/stopwords.txt', 'r');
    StopWords = textscan(StopWordsTextFile,'%s','delimiter',',');
+   WordsOfInterestTextFile = fopen('../OutsideVocabDatabases/wordsOfInterest.txt', 'r');
+   WordsOfInterest = textscan(WordsOfInterestTextFile,'%s','delimiter',',');
    
-    %% Get rid of all the characters that are not letters or numbers
+   %% Words of interest prep work
+    % Get rid of all the characters that are not letters or numbers in WordsOfInterest file.
+    for Word=1:numel(WordsOfInterest{1,1})
+        ind = find(isstrprop(WordsOfInterest{1,1}{Word,1}, 'alphanum') == 0);
+        WordsOfInterest{1,1}{Word,1}(ind)=[];
+    end
+
+    % Remove words that have zero characters in WordsOfInterest file.
+    for Word = 1:numel(WordsOfInterest{1,1})
+        if size(WordsOfInterest{1,1}{Word,1}, 2) == 0
+            WordsOfInterest{1,1}{Word,1} = ' ';
+        end
+    end
+    WordsOfInterestVocab = unique(WordsOfInterest{1,1}); % list of unique words used in the words of interest text file
+    
+    %% Syllabus prep work
+    % Get rid of all the characters that are not letters or numbers
     for Word=1:numel(CurrSylabusWords{1,1})
         ind = find(isstrprop(CurrSylabusWords{1,1}{Word,1}, 'alphanum') == 0);
         CurrSylabusWords{1,1}{Word,1}(ind)=[];
     end
 
-    %% Remove entries in words that have zero characters
+    % Remove words that have zero characters
     for Word = 1:numel(CurrSylabusWords{1,1})
         if size(CurrSylabusWords{1,1}{Word,1}, 2) == 0
             CurrSylabusWords{1,1}{Word,1} = ' ';
         end
     end
+    
+    syllabusVocab = unique(CurrSylabusWords{1,1});
+    
+    wordPresent = ismember(WordsOfInterestVocab,syllabusVocab); % binary list of which words of interest are present (1 if present, 0 if not)
+    sum(wordPresent); %tells how many words are shared between both files
+    WordsOfInterestVocab(wordPresent); % Prints out words common to both txt files
+    
+    %% Now count the number of times each word in the syllabus appears in the syllabus
+    commonWordFreq = zeros(numel(syllabusVocab), 1);
 
-    %% Now count the number of times each wordin the syllabus appears
-    uniqueWords = unique(CurrSylabusWords{1,1});
-   % ismember(StopWords,uniqueWords)
-
-    wordFreq = zeros(numel(uniqueWords), 1);
-
-    for Word = 1:numel(uniqueWords)
-        if max(uniqueWords{Word} ~= ' ')
+    for Word = 1:numel(syllabusVocab)
+        if max(syllabusVocab{Word} ~= ' ')
             for j = 1:numel(CurrSylabusWords{1,1})
-                if strcmp(CurrSylabusWords{1,1}(j), uniqueWords{Word})
-                    wordFreq(Word) = wordFreq(Word) + 1;
+                if strcmp(CurrSylabusWords{1,1}(j), syllabusVocab{Word})
+                    commonWordFreq(Word) = commonWordFreq(Word) + 1;
                 end
             end
         end
@@ -42,24 +64,24 @@ for fileNumber = 1:length(FolderOfSyllabiTxtFiles)
 
     %% Print out frequency of each word in syllabus
 
-    u_freq = unique(wordFreq);
+    u_freq = unique(commonWordFreq);
 
-    sorted_freq = sort(u_freq, 'descend');
+    sortFreq = sort(u_freq, 'descend');
 
     commonWordResults={ 'WORD' 'FREQ' '% OF WORDS'};
     numberOfPopWords = 5;
-    for Word = 1:min(numel(find(sorted_freq > 1)), numberOfPopWords)
-        ind = find(wordFreq == sorted_freq(Word));
-        commonWordResults{Word+1, 1} = uniqueWords{ind};
-        commonWordResults{Word+1, 2} = unique(wordFreq(ind));
-        commonWordResults{Word+1, 3} = sprintf('%.4f%s', unique(wordFreq(ind)/numel(CurrSylabusWords{1,1}))*100, '%');
+    for Word = 1:min(numel(find(sortFreq > 1)), numberOfPopWords)
+        ind = find(commonWordFreq == sortFreq(Word));
+        commonWordResults{Word+1, 1} = syllabusVocab{ind};
+        commonWordResults{Word+1, 2} = unique(commonWordFreq(ind));
+        commonWordResults{Word+1, 3} = sprintf('%.4f%s', unique(commonWordFreq(ind)/numel(CurrSylabusWords{1,1}))*100, '%');
     end
     
    stopWordResults={ 'WORD' 'FREQ' '% OF WORDS'};
    negativeWordResults={ 'WORD' 'FREQ' '% OF WORDS'};
    positiveWordResults={ 'WORD' 'FREQ' '% OF WORDS'};
    
-   fprintf('Total number of words in "%s" = %d \n Total number of unique words = %d \n %d Most Common Words: \n', CurrTxtFileName, numel(CurrSylabusWords{1,1}), numel(find(wordFreq)),numberOfPopWords)
+   fprintf('Total number of words in "%s" = %d \n Total number of unique words = %d \n %d Most Common Words: \n', CurrTxtFileName, numel(CurrSylabusWords{1,1}), numel(find(commonWordFreq)),numberOfPopWords)
    disp(commonWordResults);
    
    fprintf('Stop Word Usage: \n');
